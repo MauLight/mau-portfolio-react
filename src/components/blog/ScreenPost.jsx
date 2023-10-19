646468a2fc400b454a3a91652ca825496bdf0c23
MauLight <maulisseluz@gmail.com>
2023-10-19 12:43:21 -0300

    refactor: :art: Redesign screenplay view
    Rearrange of elements and new styling of view

--- src/components/blog/ScreenPost.jsx
+++ src/components/blog/ScreenPost.jsx
@@ -13,42 +13,50 @@ export const ScreenPost = ({ screenplayId }) => {
           scriptFilter.map(script => (
             <div
               key={script.id}
-              className="flex flex-col w-[100vw] sm:w-[90vw] px-2 sm:px-20"
+              className="flex flex-col w-[100vw] px-2 sm:px-20 gap-y-20 mt-10"
             >
-              <h1 className='blog-item font-primary text-black text-5xl sm:text-7xl'>{script.title}</h1>
-              <div className="border-b-2 w-2 w-full py-2"></div>
-              <div className="flex justify-start gap-x-2 my-10">
-                <p className='font-carbon text-black text-sm'>{script.date}</p>
-                <div className="border border-black rounded-full px-2">
-                  <p className='font-carbon text-black text-sm'>{script.tag}</p>
+              <div className="flex justify-between">
+                <h1 className='blog-item font-primary text-5xl sm:text-[108px] tracking-tighter bg-gradient-to-r from-primary to-danger bg-clip-text text-transparent'>{script.title}</h1>
+                <div className="flex justify-end items-end gap-x-2">
+                  <p className='font-carbon text-black text-sm'>{script.date}</p>
+
+                  <p className='font-carbon text-white bg-gradient-to-r from-primary to-danger text-sm rounded-full px-2'>{script.tag}</p>
+
                 </div>
               </div>
-              <div className={'mx-auto lg:w-[30vw] max-h-[90vh] object-contain object-center rounded-[10px] group relative overflow-hidden'}>
-                <img src={script.img} className={'w-full max-h-full object-cover hover:scale-[110%] transition-scale duration-500'} />
+
+              <div className="flex gap-x-20">
+                <div className={'mx-auto lg:w-1/3 max-h-[90vh] object-contain object-center rounded-[10px] group relative overflow-hidden'}>
+                  <img src={script.img} className={'w-full h-full object-cover hover:scale-[110%] transition-scale duration-500'} />
+                </div>
+
+
+
+                <div className="flex flex-col w-2/3 min-h-full justify-center items-center gap-y-5">
+                  <div className="flex justify-center mt-10 items-center gap-x-2">
+                    {
+                      script.genres.map((genre, i) => (
+                        <div key={i} className="bg-gradient-to-r from-primary to-danger rounded-full px-5">
+                          <p className='font-carbon text-white text-lg'>{genre}</p>
+                        </div>
+                      ))
+                    }
+                  </div>
+                  <div className="flex gap-x-5 items-center">
+                    <Quotes className='text-black w-[300px] h-auto' size={28} />
+                    <p className='font-carbon text-black text-2xl sm:text-3xl'>{script.logline}</p>
+                  </div>
+
+                </div>
               </div>
-              <div className="flex flex-col">
-                <div className="flex justify-center mt-10 items-center gap-x-2">
-                  {
-                    script.genres.map((genre, i) => (
-                      <div key={i} className="border border-black rounded-full px-2">
-                        <p className='font-carbon text-black text-sm'>{genre}</p>
-                      </div>
-                    ))
-                  }
-                </div>
-                <div className="flex gap-x-5 my-10 items-center">
-                  <Quotes className='text-black w-[300px] h-auto' size={32} />
-                  <p className='font-carbon text-black text-2xl sm:text-4xl'>{script.logline}</p>
-                </div>
-                <div className="flex my-10 justify-center items-center rounded overflow-hidden">
-                  <iframe
-                    className='rounded'
-                    src={script.pdf}
-                    width="830"
-                    height="1020"
-                    allow="autoplay"
-                  ></iframe>
-                </div>
+              <div className="flex my-10 justify-center items-center rounded overflow-hidden">
+                <iframe
+                  className='rounded w-full'
+                  src={script.pdf}
+                  width="830"
+                  height="1020"
+                  allow="autoplay"
+                ></iframe>
               </div>
             </div>
           ))
